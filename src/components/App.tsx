4b6a91e526b7ca7044a834acd353e97ed0bb88f0
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-09-08 13:35:46 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,27 @@
-import { useMemo, useSyncExternalStore } from 'react';
+import { useMemo, useReducer, useSyncExternalStore } from 'react';
 
 import PersonalDashboardPlugin from '@/main';
-import { CenterPanel } from './CenterPanel';
-import { PersonalDashboardContext } from '@/context/personalDashboardContext';
+import { AreaView } from './AreaView';
+import {
+	PersonalDashboardContext,
+	usePersonalDashboardContext,
+} from '@/context/personalDashboardContext';
+import { RightPanel } from './RightPanel';
+import { Tag } from '@/types/personalDashboardTypes';
+import { personalDashboardReducer } from '@/store/reducer';
 
 interface AppProps {
 	plugin: PersonalDashboardPlugin;
 }
+
+const items = [
+	{
+		id: '1',
+		text: 'Ask Marek about the migration',
+		time: '14:02',
+		tag: 'Task',
+	},
+];
 
 export function App({ plugin }: AppProps) {
 	const areas = useSyncExternalStore(
@@ -14,11 +29,39 @@ export function App({ plugin }: AppProps) {
 		plugin.areaStore.getSnapshot,
 	);
 
-	const ctx = useMemo(() => ({ app: plugin.app }), [plugin.app]);
+	const [state, dispatch] = useReducer(personalDashboardReducer, {
+		captureItems: [],
+	});
+
+	const ctx = useMemo(
+		() => ({ state, dispatch, app: plugin.app }),
+		[plugin.app],
+	);
+
+	const handleOnCapture = ({ text, tag }: { text: string; tag: Tag }) => {
+		dispatch({
+			type: 'ADD_CAPTURE_ITEM',
+			payload: {
+				id: crypto.randomUUID(),
+				text: text,
+				tag: tag,
+				time: new Date(Date.now()),
+			},
+		});
+	};
 
 	return (
 		<PersonalDashboardContext.Provider value={ctx}>
-			<CenterPanel areas={areas} />
+			<div className="main">
+				<AreaView areas={areas} />
+				<RightPanel
+					items={state.captureItems}
+					onCapture={handleOnCapture}
+					onFile={() => {}}
+					onArchive={() => {}}
+					onOpen={() => {}}
+				/>
+			</div>
 		</PersonalDashboardContext.Provider>
 	);
 }
